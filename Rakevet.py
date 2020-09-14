c2df24f1edea3311c4485c23573235385ce650bf
Sharon Israel <avisar@hotmail.com>
2020-09-14 19:00:59 +0300

    adding test2 label

---
+++ Rakevet.py
@@ -0,0 +1,25 @@
+print("Shalom Shahar")
+
+def number_legs(horses, cowboys):
+    return horses * 4 + 2 * cowboys
+
+#print (number_legs(3, 4))
+
+# rakevet = 12474738
+# gafrorim = rakevet * 5 + 1
+
+#print (gafrorim)
+
+
+for n in [0, 1, 2, 3, 4, 5]:
+    print(n, n*3+1)
+
+
+
+#def f(n):
+#    return n*3+1
+
+
+#for n in [0, 1, 2, 3, 4, 5]:
+#    print(n, f(n))
+
---
+++ Tali.py
@@ -0,0 +1,17 @@
+print("Shalom Tali")
+
+print(1+1)
+
+def f(n):
+    return 8*n+5
+
+print (f(2))
+
+for i in [1,2,3,4,5]:
+    print(i, f(i))
+
+
+def number_legs(horses, cowboys):
+    return horses * 4 + cowboys * 2
+
+print (number_legs(1, 2))
